df892553386d10b0cc01620caad12a6f717b942e
kritika-r <82035781+kritika-r@users.noreply.github.com>
2021-06-19 16:32:12 +0530

    VoiceSelfie App@part-2 ~ text-to-speech

--- main.js
+++ main.js
@@ -12,6 +12,25 @@ recognition.onresult= function(event){
     var Content= event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML= Content;
     console.log(Content);
-
+    
+   speak();
     
 }
+function speak(){
+    var synth= window.speechSynthesis;
+
+    var speak_data= document.getElementById("textbox").innerHTML;
+    var utterThis= new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+    
+}
+var camera= document.getElementById("camera");
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: "png",
+    quality: 90
+
+    
+});
